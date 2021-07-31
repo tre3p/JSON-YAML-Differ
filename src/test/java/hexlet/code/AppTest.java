8253061cbfa8cafa951e1100a438912eb2d72efb
Sergey Ridev <whereareyo@yahoo.com>
2021-07-31 21:24:16 +0300

    fix

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -8,28 +8,31 @@ import java.util.Map;
 
 public class AppTest {
     @Test
-    public void differTest_withFlatJson() throws Exception {
+    public void differTestWithFlatJson() throws Exception {
+
+        final int fiftyForLintTemp = 50;
+        final int twentyForLintTemp = 20;
         Map<String, Object> firstJson = new HashMap<>(Map.of(
                 "host", "hexlet.io",
-                "timeout", 50,
+                "timeout", fiftyForLintTemp,
                 "proxy", "123.234.53.22",
                 "follow", false
         ));
 
         Map<String, Object> secondJson = new HashMap<>(Map.of(
-                "timeout", 20,
+                "timeout", twentyForLintTemp,
                 "verbose", true,
                 "host", "hexlet.io"
         ));
 
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "    host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
 
         Assertions.assertEquals(Differ.generate(firstJson, secondJson), expected);
     }
