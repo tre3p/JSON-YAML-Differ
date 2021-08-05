dc68d64c9f14d7904b3b5c23a1dce8c8ce58e05a
Sergey Ridev <whereareyo@yahoo.com>
2021-08-06 00:29:52 +0300

    fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -14,22 +14,22 @@ import java.util.concurrent.Callable;
         )
 
 public final class App implements Callable<String> {
-  @Parameters(index = "0", description = "path to first file")
-  private String filepath1;
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
 
-  @Parameters(index = "1", description = "path to second file")
-  private String filepath2;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
 
-  @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
-  private String format;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
 
-  public static void main(String[] args) {
-    final var commandLineRunner = new CommandLine(new App());
-    commandLineRunner.execute(args);
-  }
+    public static void main(String[] args) {
+        final var commandLineRunner = new CommandLine(new App());
+        commandLineRunner.execute(args);
+    }
 
   @Override
   public String call() throws Exception {
-    return Differ.generate(filepath1, filepath2, format);
-  }
+        return Differ.generate(filepath1, filepath2, format);
+    }
 }
