93773b0e45851fc77153313095205f1ab7b693f3
Sergey Ridev <whereareyo@yahoo.com>
2021-08-05 23:35:16 +0300

    fix

--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.io.File;
 import java.util.Map;
 
@@ -15,12 +14,13 @@ public class Parser {
                         new TypeReference<>() {
                         });
                 return jsonResultMap;
-        } else {
+        } else if (path.endsWith(".yaml") || path.endsWith(".yml")) {
             ObjectMapper yamlObjectMapper = new ObjectMapper(new YAMLFactory());
             Map<String, Object> resultYamlMap = yamlObjectMapper.readValue(new File(path),
                     new TypeReference<>() {
                     });
             return resultYamlMap;
         }
+        return null;
     }
 }
