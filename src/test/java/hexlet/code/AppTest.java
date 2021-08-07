2d569adeac16ed1bba42d3888b7f8f26800f9073
Sergey Ridev <whereareyo@yahoo.com>
2021-08-07 18:56:09 +0300

    fix tests

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -2,56 +2,23 @@ package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AppTest {
-    private final String expectedForFlat = "{\n"
-            + "  - follow: false\n"
-            + "    host: hexlet.io\n"
-            + "  - proxy: 123.234.53.22\n"
-            + "  - timeout: 50\n"
-            + "  + timeout: 20\n"
-            + "  + verbose: true\n"
-            + "}";
+    private Path pathForFlat = Paths.get("src/test/resources/expectedForStylishPlain.json").normalize();
+    private final String expectedForFlat = Files.readString(pathForFlat);
 
-    private final String expectedForRecursive = "{\n"
-            + "    chars1: [a, b, c]\n"
-            + "  - chars2: [d, e, f]\n"
-            + "  + chars2: false\n"
-            + "  - checked: false\n"
-            + "  + checked: true\n"
-            + "  - default: null\n"
-            + "  + default: [value1, value2]\n"
-            + "  - id: 45\n"
-            + "  + id: null\n"
-            + "  - key1: value1\n"
-            + "  + key2: value2\n"
-            + "    numbers1: [1, 2, 3, 4]\n"
-            + "  - numbers2: [2, 3, 4, 5]\n"
-            + "  + numbers2: [22, 33, 44, 55]\n"
-            + "  - numbers3: [3, 4, 5]\n"
-            + "  + numbers4: [4, 5, 6]\n"
-            + "  + obj1: {nestedKey=value, isNested=true}\n"
-            + "  - setting1: Some value\n"
-            + "  + setting1: Another value\n"
-            + "  - setting2: 200\n"
-            + "  + setting2: 300\n"
-            + "  - setting3: true\n"
-            + "  + setting3: none\n"
-            + "}";
+    private Path pathToRecursive = Paths.get("src/test/resources/expectedForRecursive").normalize();
+    private final String expectedForRecursive = Files.readString(pathToRecursive);
 
-    private final String expectedForPlainOutput = "Property 'chars2' was updated. From [complex value] to false\n"
-            + "Property 'checked' was updated. From false to true\n"
-            + "Property 'default' was updated. From null to [complex value]\n"
-            + "Property 'id' was updated. From 45 to null\n"
-            + "Property 'key1' was removed\n"
-            + "Property 'key2' was added with value: 'value2'\n"
-            + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
-            + "Property 'numbers3' was removed\n"
-            + "Property 'numbers4' was added with value: [complex value]\n"
-            + "Property 'obj1' was added with value: [complex value]\n"
-            + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
-            + "Property 'setting2' was updated. From 200 to 300\n"
-            + "Property 'setting3' was updated. From true to 'none'";
+    private Path pathToPlain = Paths.get("src/test/resources/expectedForPlain").normalize();
+    private final String expectedForPlainOutput = Files.readString(pathToPlain);
+
+    public AppTest() throws IOException {
+    }
 
     @Test
     void differTestWithFlatJson() throws Exception {
