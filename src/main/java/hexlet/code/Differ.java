898250a07fc64e834c9f4c8ba477e38db6b8d6f4
Sergey Ridev <whereareyo@yahoo.com>
2021-08-11 16:35:37 +0300

    architecture fix

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class Differ {
@@ -21,12 +23,14 @@ public class Differ {
     }
 
     public static Map<String, Object> jsonParse(String path) throws IOException {
+        Path resultPath = Paths.get(path).toAbsolutePath().normalize();
         ObjectMapper jsonObjectMapper = new ObjectMapper();
-        return jsonObjectMapper.readValue(new File(path), new TypeReference<>() { });
+        return jsonObjectMapper.readValue(new File(String.valueOf(resultPath)), new TypeReference<>() { });
     }
 
     public static Map<String, Object> yamlParse(String path) throws IOException {
+        Path resultPath = Paths.get(path).toAbsolutePath().normalize();
         ObjectMapper yamlObjectMapper = new ObjectMapper(new YAMLFactory());
-        return yamlObjectMapper.readValue(new File(path), new TypeReference<>() { });
+        return yamlObjectMapper.readValue(new File(String.valueOf(resultPath)), new TypeReference<>() { });
     }
 }
