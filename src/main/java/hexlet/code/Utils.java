cb366b725ff3e7983d4ce5343d192e8451ad3f22
Sergey Ridev <whereareyo@yahoo.com>
2021-08-22 23:10:24 +0300

    fix

--- src/main/java/hexlet/code/Utils.java
+++ src/main/java/hexlet/code/Utils.java
@@ -6,7 +6,6 @@ public class Utils {
     public static String pullStringBuilderWithValues(Map<String, Object> map, StringBuilder sb) {
         for (Map.Entry<String, Object> test : map.entrySet()) {
             sb.append(test.getKey()).append(test.getValue()).append("\n");
-
         }
         return String.valueOf(sb);
     }
